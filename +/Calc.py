4fd0110879a8eac4997795a66f8d0fb8b6180bdb
Ajolote.Play <103146967+AjolotePlay@users.noreply.github.com>
2022-04-06 15:24:26 -0500

    Create Calc.py

---
+++ +/Calc.py
@@ -0,0 +1,29 @@
+# Por > Ajolote.Play
+
+de colorama import *
+
+
+# Banner
+
+impresión (""" 
+  ____      _            _       _
+ / ___|__ _| | ___ _   _| | __ _| |_ ___  _ __ 
+| |   / _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| |__| (_| | | (__| |_| | | (_| | || (_) | |   
+ \____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
+""")
+# Banner
+
+# num 1 + num 2 = suma/resta
+num1 = int(input (Fore. VERDE + "Escribe el primero número : " + Fore. LIGHTBLUE_EX + "\nEscribir aquí >"))
+num2 = int(input (Fore. LIGHTGREEN_EX + "\nEscribir el segundo número : " + Fore. LIGHTBLUE_EX + "\nEscribir aquí >"))
+# num 1 + num 2 = suma/resta
+
+
+select = int(input (Fore. LIGHTCYAN_EX + "\nEscoje que operación quieres que realize : \n[1] Suma \n[2] Resta \nEscribe Aqui >"))
+
+si selecciona == 1:
+    print(num1 + num2)
+
+si selecciona == 2:
+    print(num1 - num2)
