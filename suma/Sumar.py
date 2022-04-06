9a8389674d83c8dd7c863d1a5a80a381fa03a156
Ajolote.Play <103146967+AjolotePlay@users.noreply.github.com>
2022-04-06 15:28:24 -0500

    Create Sumar.py

---
+++ suma/Sumar.py
@@ -0,0 +1,22 @@
+# Por > Ajolote.Play
+
+de colorama import *
+
+
+# Banner
+
+impresión (""" 
+  ____      _            _       _
+ / ___|__ _| | ___ _   _| | __ _| |_ ___  _ __ 
+| |   / _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| |__| (_| | | (__| |_| | | (_| | || (_) | |   
+ \____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
+""")
+# Banner
+
+# num 1 + num 2 = suma
+num1 = int(input (Fore. VERDE + "Escribe el primero número : " + Fore. LIGHTBLUE_EX + "\nEscribir aquí >"))
+num2 = int(input (Fore. LIGHTGREEN_EX + "\nEscribir el segundo número : " + Fore. LIGHTBLUE_EX + "\nEscribir aquí >"))
+# num 1 + num 2 = suma
+
+    print(num1 + num2)
